e1460e5743fcfaff1f2b4d819b1d2de4e7af683a
Aditya Thebe <thebeaditya@gmail.com>
2024-01-07 18:24:01 +0545

    fix: k8s dependency (#1)
    * fix: k8s dependency

--- cmd/neat_test.go
+++ cmd/neat_test.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,7 +16,7 @@ limitations under the License.
 package cmd
 
 import (
-	"io/ioutil"
+	"os"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -324,7 +324,7 @@ func TestNeatEmpty(t *testing.T) {
 
 func TestNeat(t *testing.T) {
 	testsDir := "../test/fixtures"
-	testFiles, err := ioutil.ReadDir(testsDir)
+	testFiles, err := os.ReadDir(testsDir)
 	if err != nil {
 		t.Fatalf("can't list tests in: %s", testsDir)
 	}
@@ -333,12 +333,12 @@ func TestNeat(t *testing.T) {
 		fParts := strings.Split(fName, "-")
 		if fParts[1] == "raw.json" {
 			fFullName := filepath.Join(testsDir, f.Name())
-			inBytes, err := ioutil.ReadFile(fFullName)
+			inBytes, err := os.ReadFile(fFullName)
 			if err != nil {
 				t.Errorf("can't read file: %s", fFullName)
 			}
 			expFullName := filepath.Join(testsDir, fParts[0]+"-neat.json")
-			expBytes, err := ioutil.ReadFile(expFullName)
+			expBytes, err := os.ReadFile(expFullName)
 			if err != nil {
 				t.Errorf("can't read file: %s", expFullName)
 			}
